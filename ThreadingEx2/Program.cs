576bfe347fe731fa54dc373ec69b0237bd8e9be2
1NESLEiN <pnielsendk@gmail.com>
2014-09-05 10:35:17 +0200

    On my way to using Task<TResult>

--- ThreadingEx2/Program.cs
+++ ThreadingEx2/Program.cs
@@ -9,13 +9,16 @@ namespace FindSmallest
     class Program
     {
 
-        private static readonly int[][] Data = new int[][]{
-            new[]{1,5,4,2}, 
-            new[]{3,2,4,11,4},
-            new[]{33,2,3,-1, 10},
-            new[]{3,2,8,9,-1},
-            new[]{1, 22,1,9,-3, 5}
+        private static readonly int[][] Data = new int[][]
+        {
+            new[] {1, 5, 4, 2},
+            new[] {3, 2, 4, 11, 4},
+            new[] {33, 2, 3, -1, 10},
+            new[] {3, 2, 8, 9, -1},
+            new[] {1, 22, 1, 9, -3, 5}
         };
+
+        private static List<int> _smallInts = new List<int>(); 
 
         private static int FindSmallest(int[] numbers)
         {
@@ -32,33 +35,34 @@ namespace FindSmallest
                     smallestSoFar = number;
                 }
             }
-            Console.WriteLine("\t" + String.Join(", ", numbers) + "\n-> " + smallestSoFar);
             return smallestSoFar;
         }
 
         static void Main()
         {
+            List<Task> taskList = new List<Task>();
+
             foreach (int[] d in Data)
             {
-                Task findSmallestTask = Task.Run(() =>
-                {
-                    FindSmallest(d);
-                });
-
-                //To help keep the tasks from being killed, whenever the program stops
-                Console.ReadLine();
-
-                /*
-                 * Using threads
-                 * Now to use Tasks
-                Thread t = new Thread(() =>
+                Task findSmallestTask = new Task(() =>
                 {
                     int smallest = FindSmallest(d);
+                    _smallInts.Add(smallest);
                     Console.WriteLine("\t" + String.Join(", ", d) + "\n-> " + smallest);
                 });
-                t.Start();
-                 */
+
+                taskList.Add(findSmallestTask);
             }
+
+            foreach (Task t in taskList)
+            {
+                t.Start();
+            }
+            Task.WaitAll(taskList.ToArray());
+
+            Console.WriteLine("\nSmallest of all integers:");
+            int smallestOfAll = FindSmallest(_smallInts.ToArray());
+            Console.WriteLine("\t" + String.Join(", ", _smallInts.ToArray()) + "\n-> " + smallestOfAll);
         }
     }
 }
